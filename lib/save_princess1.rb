fa7c5299b89fe36a4bcad283608bb28ff170657e
Parker Boeing <parker.boeing@gmail.com>
2025-03-11 01:50:30 -0600

    Refactor: Add error handling and use helper methods for better readability/testability

--- lib/save_princess1.rb
+++ lib/save_princess1.rb
@@ -1,17 +1,41 @@
 def displayPathtoPrincess(n, grid)
-  center = n / 2
+  if validateInput(n, grid) == false
+    return
+  end
 
-  if grid[0][0] == 'p'
+  center = n / 2
+  corner = findPrincessCorner(grid)
+
+  case corner
+  when :top_left
     puts ("UP\nLEFT\n" * center)
-  elsif grid[0][-1] == 'p'
+  when :top_right
     puts ("UP\nRIGHT\n" * center)
-  elsif grid[-1][0] == 'p'
+  when :bottom_left
     puts ("DOWN\nLEFT\n" * center)
-  else
+  when :bottom_right
     puts ("DOWN\nRIGHT\n" * center)
+  else
+    raise ArgumentError, "Princess must be a 'p' in a corner of the grid."
   end
 end
 
+def findPrincessCorner(grid)
+  return :top_left     if grid[0][0]     == 'p'
+  return :top_right    if grid[0][-1]    == 'p'
+  return :bottom_left  if grid[-1][0]    == 'p'
+  return :bottom_right if grid[-1][-1]   == 'p'
+  nil
+end
+
+def validateInput(n, grid)
+  raise ArgumentError, "n must be an odd integer" if n.even?
+  raise ArgumentError, "grid is empty" if grid.empty?
+  if grid.size != n || grid.first.size != n
+    raise ArgumentError, "grid must be the same length as given integer"
+  end
+  true
+end
 
 # m = gets.to_i
 
