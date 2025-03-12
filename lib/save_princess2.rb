df8bd499a85cefd636af983c378fb7d4af060d0a
Parker Boeing <parker.boeing@gmail.com>
2025-03-11 21:14:42 -0600

    Refactor: Add error handling and use helper methods for better readability/testability in save_princess2

--- lib/save_princess2.rb
+++ lib/save_princess2.rb
@@ -1,15 +1,6 @@
 def nextMove(n, r, c, grid)
-  princessRow = nil
-  princessCol = nil
-
-  grid.each_with_index do |row, i|
-    j = row.index('p')
-    if j
-      princessRow = i
-      princessCol = j
-      break
-    end
-  end
+  validateInput(n, grid)
+  princessRow, princessCol = findPrincess(grid)
 
   if princessRow > r
     puts "DOWN"
@@ -17,7 +8,30 @@ def nextMove(n, r, c, grid)
     puts "UP"
   elsif princessCol > c
     puts "RIGHT"
-  else
+  elsif princessCol < c
     puts "LEFT"
+  elsif princessRow == r && princessCol == c
+    puts "You've found the princess!"
+  else
+    raise PrincessError, "Princess must be represented as the character p somewhere on the grid."
   end
 end
+
+def validateInput(n, grid)
+  raise GridError, "grid is empty" if grid.empty?
+  stripped_grid = grid.map { |row| row.strip }
+
+  if stripped_grid.size != n || stripped_grid.first.size != n
+    raise GridError, "grid must have the same row and column count as given integer n"
+  end
+end
+
+def findPrincess(grid)
+  grid.each_with_index do |row, i|
+    col = row.index('p')
+    return [i, col] if col
+  end
+end
+
+GridError     = Class.new(ArgumentError)
+PrincessError = Class.new(ArgumentError)
